fc60f9cca91da43733ffae9742ea8ce9fdba76e0
Todoryshyna-Maryna <todorishina.marina@mail.ru>
2020-01-04 04:36:29 +0200

    add vuetify layout and leftside navigation

--- src/server/ui/admin/src/App.vue
+++ src/server/ui/admin/src/App.vue
@@ -1,32 +1,93 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/admin">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+
+        <v-app id="inspire">
+            <!--            <nav id="top-nav">-->
+            <!--                <router-link to="/">Home</router-link>-->
+            <!--                |-->
+            <!--                <router-link to="/users">Users</router-link>-->
+            <!--            </nav>-->
+
+            <div id="nav">
+                <LeftSideNav :links="links"/>
+            </div>
+
+            <v-content>
+
+                <v-container
+                        fluid
+                >
+
+                    <router-view/>
+                </v-container>
+            </v-content>
+
+            <v-footer app>
+                <span>&copy; 2019</span>
+            </v-footer>
+
+        </v-app>
+
     </div>
-    <router-view/>
-  </div>
 </template>
 
+<script>
+    import LeftSideNav from './components/navigation/LeftSideNav';
+    import Vuetify from 'vuetify';
+
+    export default {
+        name: "Home",
+
+        data() {
+            return {
+                pageTitle: '',
+                links: [{
+                    name: 'Dashboard',
+                    path: '/',
+                    icon: 'mdi-view-dashboard'
+                },
+                    {
+                        name: 'Users',
+                        path: '/users',
+                        icon: 'mdi-view-dashboard'
+                    },]
+            }
+        },
+
+        vuetify: new Vuetify({
+            theme: {dark: true},
+        }),
+
+        components: {
+            LeftSideNav
+        }
+    }
+</script>
+
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
 
-#nav {
-  padding: 30px;
-}
+    #top-nav {
+        padding: 30px;
+    }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    #top-nav a {
+        font-weight: bold;
+        color: #2c3e50;
+    }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+    #top-nav a.router-link-exact-active {
+        color: #42b983;
+    }
+
+    .container {
+        padding: 50px;
+    }
 </style>
---
+++ src/server/ui/admin/src/components/navigation/LeftSideNav.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+        <v-navigation-drawer
+                v-model="drawer"
+                app
+                clipped
+        >
+            <ul>
+                <li
+                        class="list-item"
+                        v-for="(item, i) in links"
+                        :key="'path-'+i"
+                        avatar
+                        @click="goToPage(item.path)"
+                >
+
+                    <v-icon :class="[item.icon]">{{ item.icon }}</v-icon>
+                    {{item.name}}
+
+                </li>
+            </ul>
+        </v-navigation-drawer>
+
+        <v-app-bar
+                app
+                clipped-left
+        >
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"/>
+            <v-toolbar-title>
+                {{currentPageTitle}}
+                Dashboard
+            </v-toolbar-title>
+        </v-app-bar>
+
+    </div>
+</template>
+
+
+<script>
+    import Vuetify from 'vuetify';
+    // import {VListItem, VListItemTitle, VListItemContent} from "vuetify/lib";
+
+    export default {
+        vuetify: new Vuetify({
+            theme: {dark: true},
+        }),
+
+        data: () => ({
+            drawer: null,
+            model: 1
+
+        }),
+        methods: {
+            goToPage(path) {
+                this.$router.push(path);
+            }
+        },
+        props: {
+            source: String,
+            links: Array,
+            currentPageTitle: String
+        },
+        components: {
+            // 'v-list-item': VListItem,
+            // 'v-list-item-title': VListItemTitle,
+            // 'v-list-item-content': VListItemContent,
+        },
+
+    }
+</script>
+
+<style scoped>
+    .list-item {
+        cursor: pointer;
+        list-style: none;
+        padding: 10px 20px;
+        text-align: left;
+    }
+
+</style>
