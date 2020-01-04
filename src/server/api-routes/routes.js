b7bb28042fd3282e819059446934bb530bd1e001
Todoryshyna-Maryna <todorishina.marina@mail.ru>
2020-01-04 04:25:26 +0200

    deleted unnecessary files

--- src/server/api-routes/routes.js
+++
@@ -1,8 +0,0 @@
-import AdminDashboard from "./../ui/admin/AdminDashboard";
-
-
-export const routes = [
-    // {path: '', component: Home},
-    // {path: '/admin', component: AdminDashboard},
-    {path: '', component: AdminDashboard},
-]
