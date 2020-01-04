c981b986b20b6f5041c4d243bf14a2c0c895eaf0
Todoryshyna-Maryna <todorishina.marina@mail.ru>
2020-01-04 04:21:18 +0200

    fix static path, send CORS headers, fix routes, change indents

--- src/app.js
+++ src/app.js
@@ -4,18 +4,32 @@ const Vue = require('vue');
 const renderer = require('vue-server-renderer');
 const HTMLStream = require('vue-ssr-html-stream');
 const bodyParser = require('body-parser');
-const router = require('express').Router();
+
 
 const mongoose = require('mongoose');
 
 
 const db = require('./server/config/db')(mongoose);
+// require('./server/scripts/create-admin')(mongoose);
+
+
 const server = express();
 
+server.use(bodyParser.json());
 
 server.use(express.static(path.join(__dirname, './server/ui/admin/dist/')));
+server.use('/static', express.static(path.join(__dirname, './server/ui/admin/dist/')));
+server.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "http://localhost:8000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
-let template = require('fs').readFileSync(path.join(__dirname, './server/ui/admin/dist/index.ssr.html'), 'utf-8');
+
+let template = require('fs').readFileSync(
+    path.join(__dirname, './server/ui/admin/dist/index.ssr.html'),
+    'utf-8')
+
 let clientManifest = require('./server/ui/admin/dist/vue-ssr-client-manifest.json');
 let serverBundle = require('./server/ui/admin/dist/vue-ssr-server-bundle.json');
 
@@ -29,9 +43,20 @@ const bundleRenderer = renderer.createBundleRenderer(
     }
 );
 
-server.get('*', (req, res) => {
 
-    const context = {url: req.path, title: 'Dashboard'};
+
+const router = require('./server/api-routes/router')(server);
+
+server.use('/api/v1/admin', router);
+// server.use('/api/v1/client', router);
+
+
+server.get('/api/v1/admin/*', (req, res) => {
+
+    const context = {url: req.url, title: 'Admin panel'};
+    res.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8'
+    })
 
     bundleRenderer.renderToString(context, (err, html)=>{
         res.end(html);
@@ -40,7 +65,7 @@ server.get('*', (req, res) => {
 })
 
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
 server.listen(port, () => {
     console.log('Port: ', port);
