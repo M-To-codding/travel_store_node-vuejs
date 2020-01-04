425d2fee43e51cfeec6a0fa7c67123cbb2ec0c53
Todoryshyna-Maryna <todorishina.marina@mail.ru>
2020-01-04 04:33:49 +0200

    rename component

---
+++ src/server/ui/admin/src/views/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-app>
+        <v-content>
+            <h1>Dashboard</h1>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+    // @ is an alias to /src
+
+    export default {
+        name: 'dashboard',
+        data: () => {
+            return {
+                user: null,
+                apiData: null
+            }
+        },
+
+        components: {}
+    }
+</script>
