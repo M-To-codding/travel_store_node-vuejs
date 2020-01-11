1f3f861a536e2674e96006f6290fa3fe50f6da92
Todoryshyna-Maryna <todorishina.marina@mail.ru>
2020-01-11 10:32:14 +0200

    fix code style in entry-server.js

--- src/server/ui/admin/src/entry-server.js
+++ src/server/ui/admin/src/entry-server.js
@@ -1,34 +1,32 @@
-import { createApp } from './main'
+import { createApp } from './main';
 
-const prepareUrlForRouting = url => {
-  const { BASE_URL } = process.env
-  return url.startsWith(BASE_URL.replace(/\/$/, ''))
-    ? url.substr(BASE_URL.length)
-    : url
-}
+const prepareUrlForRouting = (url) => {
+  const { BASE_URL } = process.env;
+  return url.startsWith(BASE_URL.replace(/\/$/, '')) ? url.substr(BASE_URL.length) : url;
+};
 
-export default context => {
-  return new Promise(async (resolve, reject) => {
-    const {
-      app,
-      router,
-    } = await createApp()
+// eslint-disable-next-line consistent-return,no-async-promise-executor
+export default (context) => new Promise(async (resolve, reject) => {
+  const {
+    app,
+    router,
+  } = await createApp();
 
 
-    router.push(prepareUrlForRouting(context.url))
+  router.push(prepareUrlForRouting(context.url));
 
-    const matchedComponents = router.getMatchedComponents();
+  const matchedComponents = router.getMatchedComponents();
 
-    if (!matchedComponents.length) {
-      return reject(new Error(404));
-    }
+  if (!matchedComponents.length) {
+    return reject(new Error(404));
+  }
 
-    router.onReady(() => {
-      context.rendered = () => {
-        // eslint-disable-next-line no-console
-        // console.log(context);
-      }
-     return resolve(app)
-    }, reject)
-  })
-}
+  router.onReady(() => {
+    // eslint-disable-next-line no-param-reassign
+    context.rendered = () => {
+      // eslint-disable-next-line no-console
+      // console.log(context);
+    };
+    return resolve(app);
+  }, reject);
+});
