3026e71e4c9f03086e7cd35a212ae678c4917ffa
Marina <marina.todorishina@gmail.com>
2020-01-15 19:03:12 +0200

    install bcrypt, jsonwebtoken, start to add auth in server

--- src/server/models/User.js
+++ src/server/models/User.js
@@ -1,31 +1,41 @@
 module.exports = (mongoose, uniqueValidator) => {
 
-    let UserSchema = new mongoose.Schema({
-            name: {
-                type: String,
-                lowercase: true,
-                required: [true, "can't be blank"],
-                match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
-                index: true
-            },
-            email: {
-                type: String,
-                lowercase: true,
-                required: [true, "can't be blank"],
-                match: [/\S+@\S+\.\S+/, 'is invalid'],
-                index: true
-            },
+  const jwt = require('jsonwebtoken');
 
-            password: String,
-            image: String,
-            role: String,
-        },
-        {
-            timestamps: true
-        });
+  let UserSchema = new mongoose.Schema({
+      name: {
+        type: String,
+        lowercase: null,
+        required: [true, "can't be blank"],
+        match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+        index: true
+      },
+      email: {
+        type: String,
+        lowercase: true,
+        required: [true, "can't be blank"],
+        match: [/\S+@\S+\.\S+/, 'is invalid'],
+        index: true
+      },
 
-    // UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
+      password: String,
+      image: String,
+      role: String,
+      isAdmin: Boolean
+    },
+    {
+      timestamps: true
+    });
 
-    mongoose.model('User', UserSchema);
+  // UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
+
+
+  UserSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({_id: this._id, isAdmin: this.isAdmin}, config.get('myprivatekey')); //get the private key from the config file -> environment variable
+    return token;
+  }
+
+
+  mongoose.model('User', UserSchema);
 
 }
