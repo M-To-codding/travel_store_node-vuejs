5db032d5d1011448a330dc62fefa57bd069babbf
Todoryshyna-Maryna <todorishina.marina@mail.ru>
2020-01-15 11:31:20 +0200

    add sign in page

---
+++ src/server/ui/admin/src/views/SignIn.vue
@@ -0,0 +1,105 @@
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+  <v-container
+      fluid
+      justify="center"
+      style="padding-top: 50px;">
+
+    <v-row justify="center">
+      <h1 class="title">Sign in</h1>
+    </v-row>
+
+    <v-col
+        class="centered-block"
+        cols="8" sm="6" md="4">
+
+      <v-form
+          ref="form"
+          v-model="valid"
+          lazy-validation>
+
+        <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required></v-text-field>
+
+        <v-text-field
+            v-model="password"
+            :rules="passwRules"
+            label="Password"
+            type="password"
+            required></v-text-field>
+
+        <v-btn
+            :disabled="!valid"
+            color="success"
+            class="mr-4"
+            @click="signIn">
+          Sign in
+        </v-btn>
+        <v-btn x-small
+               color="secondary"
+               @click="goTo('/sign-up')"
+               dark>Sign up
+        </v-btn>
+
+      </v-form>
+    </v-col>
+
+  </v-container>
+
+</template>
+
+<script>
+  export default {
+    name: "SignIn",
+
+    data: () => {
+      return {
+        valid: true,
+        name: '',
+
+        nameRules: [
+          v => !!v || 'Name is required',
+          v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+        ],
+
+        email: '',
+        emailRules: [
+          v => !!v || 'E-mail is required',
+          v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+        ],
+
+        password: '',
+        passwRules: [],
+      };
+
+    },
+
+    methods: {
+      signIn() {
+        this.validate();
+      },
+      goTo(path) {
+        this.$router.push(path);
+      },
+
+      validate() {
+        if (this.$refs.form.validate()) {
+          this.snackbar = true
+        }
+      },
+    },
+  }
+</script>
+
+<style scoped>
+  .title {
+    padding: 30px 0;
+  }
+
+  .centered-block {
+    margin: auto;
+    justify-content: center;
+  }
+</style>
