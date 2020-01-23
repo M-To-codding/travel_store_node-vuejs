b60aabb3875ae28438a25ac75916ff6a59e2803d
marina <marina.todorishina@gmail.com>
2020-01-23 03:08:04 +0200

    fix styles, change indents

--- src/server/ui/admin/src/views/Dashboard.vue
+++ src/server/ui/admin/src/views/Dashboard.vue
@@ -1,23 +1,45 @@
-<template>
+<template  xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
     <v-app>
-        <v-content>
-            <h1>Dashboard</h1>
-        </v-content>
+        <v-container
+                fluid
+                justify="center"
+                style="padding-top: 50px;"
+        >
+
+            <v-row justify="center">
+                <h1 class="title">Dashboard</h1>
+            </v-row>
+
+        </v-container>
+
     </v-app>
 </template>
 
 <script>
-    // @ is an alias to /src
+  // @ is an alias to /src
 
-    export default {
-        name: 'dashboard',
-        data: () => {
-            return {
-                user: null,
-                apiData: null
-            }
-        },
+  export default {
+    name: 'dashboard',
+    data: () => {
+      return {
+        user: null,
+        apiData: null
+      }
+    },
 
-        components: {}
-    }
+    components: {}
+  }
 </script>
+
+
+<style scoped>
+    .title {
+        padding: 30px 0;
+        color: #fff;
+    }
+
+    .centered-block {
+        margin: auto;
+        justify-content: center;
+    }
+</style>
