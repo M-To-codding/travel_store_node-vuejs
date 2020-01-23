1430a36770ff84d53c01b5e53d7d631b000e921c
marina <marina.todorishina@gmail.com>
2020-01-23 03:08:52 +0200

    create auth methods: sign in, sign up and sign out

--- src/server/ui/admin/src/actions/auth.js
+++ src/server/ui/admin/src/actions/auth.js
@@ -1,14 +1,58 @@
-function register(axios, data) {
-  axios.auth(data);
+function register(axios, user) {
+  return axios.post('http://localhost:8080/api/v1/admin/register', user)
+    .then((res) => {
+      // const token = res
+      // localStorage.setItem('user-token', token);
+      // console.log('response', res);
+      // eslint-disable-next-line no-console
+      console.log('response', res);
+      return Promise.resolve(res);
+    })
+    .catch((err) => err.response);
 }
 
-function signIn(axios, data) {
-  axios.auth(data);
+function signIn(axios, user) {
+  return axios.post('http://localhost:8080/api/v1/admin/sign-in', user)
+    .then((res) => {
+      const { token } = res.data;
+
+      if (token) {
+        localStorage.setItem('user-token', token);
+      }
+
+      return Promise.reject(res);
+    })
+    .catch((err) => {
+      if (err && err.response) {
+        localStorage.removeItem('user-token');
+        return err.response;
+      }
+      return err;
+    });
+}
+
+function signOut(axios) {
+  const data = { token: { tokenKey: localStorage.getItem('user-token') } };
+
+  return axios.post('http://localhost:8080/api/v1/admin/sign-out', data)
+    .then(
+      () => {
+        localStorage.removeItem('user-token');
+      },
+    )
+    .catch((err) => {
+      if (err.response) {
+        return err.response;
+      }
+
+      return err;
+    });
 }
 
 const auth = {
   register,
   signIn,
-}
+  signOut,
+};
 
 export default auth;
