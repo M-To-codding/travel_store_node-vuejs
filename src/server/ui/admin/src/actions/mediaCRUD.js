b8e000966cacf71645b71645c3660900e65de643
marina <marina.todorishina@gmail.com>
2020-02-22 02:54:14 +0200

    rename media CRUD file, remove unnecessary images

--- src/server/ui/admin/src/actions/mediaCRUD.js
+++ src/server/ui/admin/src/actions/mediaCRUD.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
 
 const headers = {
-  // accept: 'application/json',
-  // 'Accept-Language': 'en-US,en;q=0.8',
+  accept: 'application/json',
+  'Accept-Language': 'en-US,en;q=0.8',
   'Content-Type': 'multipart/form-data',
 };
 
 function uploadFile(file) {
   return axios
-    .post('http://localhost:8080/api/v1/admin/file-upload', { file }, { headers })
+    .post('http://localhost:8080/api/v1/admin/file-upload', file, { headers })
     .then((res) => {
       // eslint-disable-next-line no-console
       console.log('response', res);
